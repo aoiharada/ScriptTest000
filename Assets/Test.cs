46c94f0fb94d907feb57929cb6a2347f53098cf1
harada aoi <91741127+aoiharada@users.noreply.github.com>
2021-12-22 20:11:02 +0900

    課題を作成

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,30 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+        private int hp = 100;          
+        private int power = 25; 
+
+        
+        public void Attack()
+        {
+                Debug.Log( this.power + "のダメージを与えた" );
+        }
+
+        
+        public  void Defence(int damage)
+        {
+                Debug.Log( damage+"のダメージを受けた" );
+                
+                this.hp -= damage;
+        }
+
+}
+
 public class Test : MonoBehaviour
 {
 
-       
-       void HelloName(string name)
-       {
+        void Start ()
+        {
+            int[] array = {10,20,30,40,50};
+            for(int i=0;i<5;i++){
+                Debug.Log(array[i]);
+            for( int i = 4; i >= 0 ; i-- ) {
+               Debug.Log( array[i] );
+            }
+        }
+               
+                Boss lastboss = new Boss ();
 
-                Debug.Log ("Hello, " + name);
+                
+                lastboss.Attack();
+                
+                lastboss.Defence(3);
+        }
 
-       }
-       
-
-       void Start()
-       {
-
-             HelloName ("Unity");
-
-       }
-    
-
-
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
- }
+        void Update ()
+        {
+
+        }
+}
